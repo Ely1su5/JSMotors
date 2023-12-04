915a96987cb9734dd804294a2dce846c588196e0
El_y1sus <jesusitoduran23@gmail.com>
2023-12-04 19:34:43 -0400

    push

---
+++ JS/test_1.js
@@ -0,0 +1,25 @@
+// let num = 10
+// let num2 = 5
+
+// console.log(num + num2)
+// console.log(num * num2)
+// console.log(num - num2)
+// console.log(num / num2)
+
+let bottom = document.getElementById("btn-r")
+
+bottom.addEventListener("click", () =>{
+    window.alert("Trolleado, este boton no sirve para nada")
+})
+
+// Igual igual ==
+// <= menor o igual que
+// >= mayor o igual que
+// = igual
+// > mayor
+// < menor
+// != diferente que
+
+//2==2&&3==4
+
+//2==2||3==4
